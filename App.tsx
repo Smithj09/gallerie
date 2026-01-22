3f5c79c1492507850ba2d5da1af15c087aed6176
Smith Junior Prud'homme <sjprudhome@joinhep.org>
2026-01-23 00:27:33 +0800

    all

--- App.tsx
+++ App.tsx
@@ -8,11 +8,13 @@ const App: React.FC = () => {
       {/* Mock Header for Context */}
       <header className="bg-white border-b border-slate-100 py-4 px-6 sticky top-0 z-40">
         <div className="max-w-7xl mx-auto flex items-center justify-between">
-          <div className="flex items-center gap-3">
-            <div className="w-10 h-10 bg-[#0D3156] rounded-full flex items-center justify-center text-[#FFC600] font-black italic">
-              {/* Logo  */}
-            </div>
-          </div>
+            <div className="flex-shrink-0 cursor-pointer py-2">
+              <img 
+                src="https://i.postimg.cc/5y2pkLJ9/logo.jpg" 
+                alt="Logo" 
+                className="h-20 w-auto object-contain" 
+              />
+            </div>  
           
           
 
@@ -25,7 +27,7 @@ const App: React.FC = () => {
       {/* Hero Mini Section */}
       <div className="bg-[#0D3156] py-12 px-6 text-center">
         <h2 className="text-white text-3xl md:text-5xl font-black uppercase mb-4 tracking-tight">
-          Portfolio de nos <span className="text-[#FFC600]">Solutions</span>
+          Portfolio de nos <span className="text-[#FFC600]">Réalisations</span>
         </h2>
         <p className="text-slate-400 text-xs md:text-sm uppercase tracking-[0.3em] max-w-2xl mx-auto font-medium">
           Installation, entretien et performance solaire à travers tout le territoire.
@@ -40,9 +42,14 @@ const App: React.FC = () => {
       <footer className="bg-slate-50 border-t border-slate-200 py-12 px-6">
         <div className="max-w-7xl mx-auto flex flex-col items-center">
           <div className="flex items-center gap-3 mb-8">
-            <div className="w-12 h-12 bg-[#0D3156] rounded-full flex items-center justify-center text-[#FFC600] font-black italic text-xl">
-              
-            </div>
+          <div className="flex-shrink-0 cursor-pointer  flex items-center justify-center">
+                   
+            <img 
+            src="https://i.postimg.cc/5y2pkLJ9/logo.jpg" 
+            alt="Solar Logo" 
+            className="h-40 w-auto object-contain" 
+          />
+          </div>
           </div>
           
           <p className="text-[#4A6278] text-[10px] font-bold uppercase tracking-[0.4em] opacity-50 text-center">
